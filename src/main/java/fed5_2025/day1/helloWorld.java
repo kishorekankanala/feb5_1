5bb7a13444141840af38bb2e7cf240c38918495a
kanka@kishore <kankanalakishore9@gmail.com>
2025-02-05 23:00:10 -0600

    added extended program

--- src/main/java/fed5_2025/day1/helloWorld.java
+++ src/main/java/fed5_2025/day1/helloWorld.java
@@ -3,5 +3,9 @@ package fed5_2025.day1;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World this is first class");
+
+        for(int i = 1; i<=5;i++){
+            System.out.println("hello This is : "+i);
+        }
     }
 }
