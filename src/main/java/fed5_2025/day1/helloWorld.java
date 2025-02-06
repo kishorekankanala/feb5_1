96fa9322c42318b1d307da2533815626f1f49701
kanka@kishore <kankanalakishore9@gmail.com>
2025-02-06 07:51:53 -0600

    added extended program

--- src/main/java/fed5_2025/day1/helloWorld.java
+++ src/main/java/fed5_2025/day1/helloWorld.java
@@ -2,7 +2,7 @@ package fed5_2025.day1;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World this is first class");
+        System.out.println("Hello World this is first class to me");
 
         for(int i = 1; i<=5;i++){
             System.out.println("hello This is : "+i);
---
+++ src/main/java/fed5_2025/day1/multiplicationOfNum.java
@@ -0,0 +1,11 @@
+package fed5_2025.day1;
+
+public class multiplicationOfNum {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b =10;
+        float multi = a*b;
+        System.out.println(a+" *"+b+" ="+multi);
+    }
+}
